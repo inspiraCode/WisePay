e7d89c3f2f13c58272e976ee20072e9b06de2064
Alfredo Pacheco <apacheco@capsonic.com>
2015-05-17 22:57:01 -0600

    First Yeoman structure

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wisePayApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wisePayApp
+ */
+angular.module('wisePayApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
